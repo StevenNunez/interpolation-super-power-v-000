ee80ad67cb2c2d814968dd7609bd914803b8690d
Steven Nunez <steven.nunez@gmail.com>
2019-02-20 22:03:01 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  colors.map.each_index do |i|
-    "#{colors[i].[0].upcase}: colors[i]"
-  end
+  puts colors.map.each_index do |i|
+    "#{colors[i][0].upcase}: colors[i]"
+  end.joins(", ")
 end
